b7e931e5e3c337e6a7f552537ba55e2b04ff20a1
PieroBellanova <80577623+PieroBellanova@users.noreply.github.com>
2021-03-15 17:20:56 +0100

    VirtualLabTour

---
+++ assets/playbar/playbarScript.js
@@ -0,0 +1,52 @@
+cp.playbarAssetArr = 
+[
+	'AudioOff',
+	'AudioOn',
+	'BackGround',
+	'Backward',
+	'Color',
+	'ColorSmall',
+	'CC',
+	'Exit',
+	'FastForward',
+	'FastForward1',
+	'FastForward2',
+	'Forward',
+	'Glow',
+	'GlowSmall',
+	'Height',
+	'InnerStroke',
+	'InnerStrokeSmall',
+	'Play',
+	'Pause',
+	'Progress',
+	'Rewind',
+	'Shade',
+	'ShadeSmall',
+	'Stroke',
+	'StrokeSmall',
+	'Thumb',
+	'ThumbBase',
+	'TOC'
+];
+cp.playbarTooltips = 
+{
+	AudioOff : "Audio an ",
+	AudioOn : "Audio aus ",
+	Backward : "Zurück ",
+	CC : "Bilduntertitel ",
+	Exit : "Beenden ",
+	FastForward : "Zweifache Vorspulgeschwindigkeit ",
+	FastForward1 : "Vierfache Vorspulgeschwindigkeit ",
+	FastForward2 : "Normale Geschwindigkeit ",
+	Forward : "Weiter ",
+	Play : "Abspielen ",
+	Pause : "Anhalten ",
+	Rewind : "Zurückspulen ",
+	TOC : "Inhaltsverzeichnis ",
+	Info : "Informationen ",
+	Print : "Drucken "
+};
+cp.handleSpecialForPlaybar = function(playbarConstruct)
+{
+}
